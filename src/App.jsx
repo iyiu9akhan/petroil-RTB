3c48b759061552f1ea333f56199da22a7e92ce5d
iyiu9akhan <iyiu9akhan@gmail.com>
2025-05-31 15:18:39 +0600

    windows: services section top portion solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Banner from './components/banner/Banner'
 import Header from './components/header/Header'
+import Services from './components/services/Services'
 import Slider from './components/slider/SliderImg'
 import HeaderSocial from './components/top_header/HeaderSocial'
 import Welcome from './components/welcome/welcome'
@@ -14,6 +15,7 @@ function App() {
      <Banner></Banner>
      <Welcome></Welcome>
      <Slider></Slider>
+     <Services></Services>
     </>
   )
 }
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import services_one from '../../assets/services/services_one.png'
+import Button from '../layout/button'
+
+function Services() {
+  return (
+    <div>
+        <div className='flex justify-end'>
+            <div className='w-[509px] mr-[61px] flex flex-col justify-center'>
+                <h1 className='font-primary font-bold text-[64px] text-black'>Our Services</h1>
+                <p className='w-[405px] font-primary font-medium text-base text-[#6c6c6c]'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+            </div>
+            <div className="bg-[url('./src/assets/services/services_one.png')] relative before:content-[''] before:absolute before:inset-0 before:bg-black/60">
+                <h2 className='font-primary font-bold text-[36px] text-white mt-[147px] ml-[116px] mr-[153px] mb-[19px] relative'>Modern natural oil and gas refineries.</h2>
+                <Button className='bg-red border-none ml-[116px] mb-[139px] cursor-pointer relative'>learn more</Button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Services
